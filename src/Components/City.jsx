bb419d6dbf1386b3704a80d7c6fe946415a75bae
Bishal <[bsal.stha123@gmail.com]>
2022-09-08 10:37:43 +0545

    Save in local Storage

--- src/Components/City.jsx
+++ src/Components/City.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './City.css'
 
-const City =(props) => {
+const City = (props) => {
     return (
         <p>Hello, I am in London, {props.city}. It is {props.features}.</p>
 
--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -2,20 +2,53 @@ import React, { useState } from "react";
 import "./Todo.css";
 
 const Todo = () => {
-  const [input, setInput] = useState([]);
-  const [todos, setTodos] = useState([
-    "Take a walk.",
-    "Buy grocery.",
-    "Read a book.",
-    "Take a nap.",
-  ]);
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')) || []);
+  
 
   console.log(input);
   console.log(todos);
 
+  //Set item to local Storage
+  const saveLocalTodos = (todo) => {
+    
+    if(localStorage.getItem('todos') === null){
+      localStorage.setItem('todos', JSON.stringify([]));
+    }
+    else{
+       let todoLocal = JSON.parse(localStorage.getItem('todos'));
+       todoLocal.push(todo);
+       localStorage.setItem('todos', JSON.stringify(todoLocal));
+    }
+  }
+
+  //CREATE: Create a new todo
   function handleSubmit() {
-    setTodos([...todos, input]);
+    setTodos([...todos, input])
+    
+    saveLocalTodos(input)
+    setInput('')
+  }
+  
+  //UPDATE: Update the individual items in the array having index
+  function updateTodo(index){
+    const newTodos = [...todos];
+    newTodos.splice(index, 1, input);
+    setTodos(newTodos);
     setInput('');
+  }
+
+  //DELETE: Delete the individual items in the array having index
+  function deleteTodo(index){
+    const newTodos=[...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
+  //REMOVE ALL: Remove all the items in the array
+  function removeAll(){
+    setTodos([])
+    localStorage.setItem('todos', JSON.stringify([]))
   }
 
   return (
@@ -42,16 +75,45 @@ const Todo = () => {
           />
         </form>
       </div>
+      
       <div className="todo-list">
         <h3>Task List</h3>
         <ul>
           {todos.map((todo) => (
-            <li>{todo}</li>
+            <li>{todo}
+               <button
+                  className='update'
+                  onClick={e => updateTodo(todos.indexOf(todo))}
+              >
+                   Update
+               </button>
+
+               <button
+                  className='delete'
+                  onClick={() => deleteTodo(todos.indexOf(todo))}
+                   >
+                   Delete 
+                </button>
+            </li>
           ))}
         </ul>
       </div>
+     
+      <div className="todo-removal">
+            <button
+                className='remove'
+                onClick={removeAll}
+                >
+                Remove All
+           </button>
+       </div>
     </div>
+    
+  
+
   );
+  
 };
 
 export default Todo;
+
