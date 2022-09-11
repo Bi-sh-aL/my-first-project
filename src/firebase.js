dca09991c958673fe7b7c1e6a69b2bbd93345f74
Bi-sh-aL <bsal.stha123@gmail.com>
2022-09-11 11:11:49 +0545

    Firebase Config

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCUPKFHrUoAbF4krAKn9K3FHyWll0fMPzc",
+    authDomain: "todo-app-82d14.firebaseapp.com",
+    projectId: "todo-app-82d14",
+    storageBucket: "todo-app-82d14.appspot.com",
+    messagingSenderId: "1011690895735",
+    appId: "1:1011690895735:web:b8fb9ec3fb7b3faa014972",
+    measurementId: "G-N914461RJF"
+  };
+
+const firebase = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+export default db;
